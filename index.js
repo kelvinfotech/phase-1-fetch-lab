26e681109bf73031a8fd549dde4b82ffb0b1f1ca
Mwenda <kelvinfotech@gmail.com>
2024-04-01 11:19:40 +0300

    Fetch Lab

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Sending fetch request to the API and returning the promise
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      // Checking if response is successful
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      // Parsing JSON data from response and returning it
+      return response.json();
+    })
+    .then(data => {
+      // Rendering book titles
+      renderBooks(data);
+    })
+    .catch(error => {
+      // Handling errors
+      console.error('There was a problem fetching the books:', error);
+      throw error; // Re-throwing the error to be caught in the calling context
+    });
 }
 
 function renderBooks(books) {
@@ -13,5 +30,8 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks()
+    .catch(error => {
+      console.error('Error fetching books:', error);
+    });
 });
